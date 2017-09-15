8b096ab0cce045a9ec151c4d2d014d4c8e39eb62
shaunsnowden <ssnowden07@gmail.com>
2017-09-14 18:40:17 -0600

    Map working with artist search

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -15,20 +15,123 @@ $(document).ready(function(){
   var searches = [""];
 
 
+  // basic artist lookup & get to main genre
+  var musicGraphKey = "15ad1c68b6d53829ad83e71ac3c5a746";
+
+  
+  function callMusicGraph(){
+    $.ajax({
+      type: "GET",
+      url: "http://api.musicgraph.com/api/v2/artist/search?api_key=" + musicGraphKey + "&name=" + artists,
+    }).done(function(response){
+      var musicGraphGenre = response.data[0].main_genre + " Music";
+      console.log(musicGraphGenre);
+      
+      // Edit DOM with artist name
+      $("#venuesHeader").text("You are stalking " + artists + ", here are some places you may enjoy skulking about:");
+
+      // Update map from artist
+      var pos;
+      var userCords;
+      var tempMarkerHolder = [];
+
+      if (navigator.geolocation) {    
+        function error(err) {
+          console.warn('ERROR(' + err.code + '): ' + err.message);
+        }
+
+        function success(pos){
+          userCords = pos.coords;
+          console.log(userCords);
+          console.log(userCords.latitude);
+          //return userCords;
+
+          var mapOptions = {
+            zoom: 12,
+            // Center on geolocation
+            center: new google.maps.LatLng(userCords.latitude, userCords.longitude),
+            panControl: true,
+            panControlOptions: {
+              position: google.maps.ControlPosition.BOTTOM_LEFT
+            },
+            zoomControl: true,
+            zoomControlOptions: {
+              style: google.maps.ZoomControlStyle.LARGE,
+              position: google.maps.ControlPosition.RIGHT_CENTER
+            },
+            scaleControl: true
+          };
+
+          // Fire up Google Maps
+          map = new google.maps.Map(document.getElementById('relatedVenues'), mapOptions);
+
+          var locationLatLong = {lat: userCords.latitude, lng: userCords.longitude};
+
+          infowindow = new google.maps.InfoWindow();
+          var service = new google.maps.places.PlacesService(map);
+          service.nearbySearch({
+            location: locationLatLong,
+            radius: 10000,
+            keyword: [musicGraphGenre],
+            type: ['bars']
+          }, callback2);
+
+          function callback2(results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK) {
+              for (var i = 0; i < results.length; i++) {
+                createMarker(results[i]);
+                // console.log(results[i]);
+                // console.log(musicGraphGenre);
+              }
+            }
+          }
+
+          function createMarker(place) {
+            var placeLoc = place.geometry.location;
+            var marker = new google.maps.Marker({
+              map: map,
+              position: place.geometry.location
+            });
+
+            google.maps.event.addListener(marker, 'click', function() {
+              infowindow.setContent(place.name);
+              infowindow.open(map, this);
+            });
+          }
+
+        } //closes "success"
+
+        // Get the user's current position
+        navigator.geolocation.getCurrentPosition(success, error);
+      } else {
+          alert('Geolocation is not supported in your browser');
+      }  //closes all if else
+
+    }); 
+
+    } //Closes function musicGraph
+  
+
   	
   		  	
   	var artists = "";
+    var location = "";
 
   
 
 
-  	 $("#addArtist").on("click", function(event) {
+  	$("#addArtist").on("click", function(event) {
   	 	event.preventDefault()
-      console.log("Button was clicked");
 
-      //Grabbed values from text boxes
-      var artists = $("#artist-input").val().trim();
-      console.log("artists", artists)
+      //Grabbed & Clear values from text boxes
+      artists = $("#artist-input").val().trim();
+      // var location = $("#location-input").val().trim();
+      $("#artist-input").val("");
+      // $("#location-input").val("");
+
+
+      // Music Graph Call
+      callMusicGraph();
 
 
       console.log('before', searches)
@@ -37,7 +140,7 @@ $(document).ready(function(){
       console.log('after', searches)
       
       database.ref("searches").set(searches)
-});
+    });
       
      
 
